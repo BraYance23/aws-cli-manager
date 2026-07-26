b8dbd70c31a31955ec076fd389957e442fc63173
BraYance23 <brayanyance35@gmail.com>
2026-07-25 20:36:46 -0500

    feat: add custom exception classes for centralized error handling

---
+++ exceptions.py
@@ -0,0 +1,40 @@
+
+
+class AWSError(Exception):
+
+    def __init__(self,code:str):
+        self.code = code
+
+class AplicationException(Exception):
+    pass
+
+class ResourceNotFound(Exception):
+
+    def __init__(self,region:str="",sg_id:str=""):
+        self.region = region
+        self.sg_id = sg_id
+
+class NoEC2Instances(ResourceNotFound):
+    pass
+
+class NoSecurityGroups(ResourceNotFound):
+    pass
+
+class NoIngressRules(ResourceNotFound):
+    pass
+
+class NoEgressRules(ResourceNotFound):
+    pass
+
+class NoKeyPairs(ResourceNotFound):
+    pass
+
+class UserCancelOperation(AplicationException):
+    pass
+
+class CredentialsError(AWSError):
+    pass
+
+class CredentialsNotFound(AWSError):
+    pass
+
